e079b04ea7a94af1fda805359438451d897b2c05
swapnilxi <swapnilg671@gmail.com>
2024-04-01 00:32:16 +0530

    Initial Commit

---
+++ ios-learn/ios_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_learnApp.swift
+//  ios-learn
+//
+//  Created by Swap-Dev on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
