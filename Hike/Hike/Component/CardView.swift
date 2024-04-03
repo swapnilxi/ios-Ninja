399465cf0bb0fdf2ecb065b76923315f10b2ca82
swapnilxi <swapnilg671@gmail.com>
2024-04-03 15:59:20 +0530

    Hike-boilerplate

--- Hike/Hike/Component/CardView.swift
+++ Hike/Hike/Component/CardView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+            .fill(
+             LinearGradient(colors: [Color("ColorIndigoMedium"),
+                  Color("ColorSalmonLight")],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing)
+            )
+            .frame(width: 256, height: 256)
+            
+             Image("image-1")
+                .resizable()
+            .scaledToFit()
+        }
     }
 }
 
