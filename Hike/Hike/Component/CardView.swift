c48271a13133ac9904f0bf52f08cb8f68fb8bb8c
swapnilxi <swapnilg671@gmail.com>
2024-04-04 16:16:35 +0530

    hike-app/header

--- Hike/Hike/Component/CardView.swift
+++ Hike/Hike/Component/CardView.swift
@@ -10,19 +10,63 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
-            Circle()
-            .fill(
-             LinearGradient(colors: [Color("ColorIndigoMedium"),
-                  Color("ColorSalmonLight")],
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing)
-            )
-            .frame(width: 256, height: 256)
-            
-             Image("image-1")
-                .resizable()
-            .scaledToFit()
-        }
+            CustomBackgroudView()
+            VStack {
+                // MARK: - HEADER
+                
+                VStack(alignment: .leading) {
+                  HStack {
+                    Text("Hiking")
+                      .fontWeight(.black)
+                      .font(.system(size: 52))
+                      .foregroundStyle(
+                        LinearGradient(
+                          colors: [
+                            .customGrayLight,
+                            .customGrayMedium],
+                          startPoint: .top,
+                          endPoint: .bottom)
+                      )
+                    
+                    Spacer()
+                    
+                    Button {
+                      // ACTION: Show a Sheet
+                      print("The button was pressed.")
+                    } label: {
+                      CustomButtonView()
+                    }
+                    
+                  }
+                  
+                  Text("Fun and enjoyable outdoor activity for friends and families.")
+                    .multilineTextAlignment(.leading)
+                    .italic()
+                    .foregroundColor(.customGrayMedium)
+                } //: HEADER
+                .padding(.horizontal, 30)
+                
+                // Mark- main content
+                ZStack {
+                    Circle()
+                    .fill(
+                     LinearGradient(colors: [Color("ColorIndigoMedium"),
+                          Color("ColorSalmonLight")],
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing)
+                    )
+                    .frame(width: 256, height: 256)
+                    
+                     Image("image-1")
+                        .resizable()
+                    .scaledToFit()
+                }
+                //Mark-footer
+                
+                
+            }//:VStack
+        }//CARD
+        .frame(width:320,height: 570)
     }
 }
 
