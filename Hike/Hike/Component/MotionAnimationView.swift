934d7c8b5fb4250d8b79976270595f0ac1789687
swapnilxi <swapnilg671@gmail.com>
2024-04-07 04:09:02 +0530

    hike-app/gradient

---
+++ Hike/Hike/Component/MotionAnimationView.swift
@@ -0,0 +1,90 @@
+//
+//  MotionAnimationView.swift
+//  Hike
+//
+//  Created by Swap-Dev on 4/6/24.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    //Mark- Properties
+    //Mark- Random Circle Number
+    @State private var randomCircle:Int=Int.random(in: 6...12)
+    @State private var isAnimating: Bool = false
+
+    
+    //Mark- Random Coordinates
+    func randomCoordinate() -> CGFloat{
+        return CGFloat.random(in: 0...256)
+        
+    }
+    // 2. RANDOM SIZE
+    func randomSize() -> CGFloat {
+      return CGFloat(Int.random(in: 4...80))
+    }
+    
+    // 3. RANDOM SCALE
+    func randomScale() -> CGFloat {
+      return CGFloat(Double.random(in: 0.1...2.0))
+    }
+    
+    // 4. RANDOM SPEED
+    func randomSpeed() -> Double {
+      return Double.random(in: 0.05...1.0)
+    }
+    
+    // 5. RANDOM DELAY
+    func randomDelay() -> Double {
+      return Double.random(in: 0...2)
+    }
+    
+    
+    
+    
+    var body: some View {
+        ZStack {
+            ForEach(0...randomCircle, id: \.self) { item in
+                Circle()
+                    .foregroundColor(.white)
+                    .opacity(0.25)
+                    .frame(width: randomSize())
+                    .position(
+                        x:randomCoordinate(),
+                        y:randomCoordinate()
+                    )
+                    .scaleEffect(isAnimating ?randomScale() :1)
+                    .onAppear(
+                        perform: {
+                            withAnimation(
+                            .interpolatingSpring(stiffness: 0.25, damping: 0.25)
+                            .repeatForever()
+                            .speed(randomSpeed())
+                            .delay(randomDelay())
+                        )//withanimation
+                        {
+                            isAnimating=true
+                        }
+                    }
+                    )//onAppear
+            }
+        } //:zstack
+        .frame(width: 256, height: 256)
+        .mask(Circle())
+        .drawingGroup() //for metal rendering
+
+    }
+}
+
+#Preview {
+//    ZStack {
+//        Color.teal.ignoresSafeArea()
+//        MotionAnimationView()
+//    }
+    MotionAnimationView()
+        .background(
+            Circle()
+                .fill(.teal)
+        
+        )
+}
---
+++ Hike/Hike/Utilities/GradientButtonStyle.swift
@@ -0,0 +1,29 @@
+//
+//  GradientButtonStyle.swift
+//  Hike
+//
+//  Created by Swap-Dev on 4/4/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct GradientButton: ButtonStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    configuration
+      .label
+      .padding(.vertical)
+      .padding(.horizontal, 30)
+      .background(
+        // Conditional Statement with Swift Ternary Operator
+        // Condition ? A : B
+        configuration.isPressed ?
+        // A: When User pressed the Button
+        LinearGradient(colors: [.customGrayMedium, .customGrayLight], startPoint: .top, endPoint: .bottom)
+        :
+        // B: When the Button is not pressed
+        LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+      )
+      .cornerRadius(40)
+  }
+}
