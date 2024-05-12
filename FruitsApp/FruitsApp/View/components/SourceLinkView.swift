570111b6ab83f43d4548552cb4c70b3ec65de331
swapnilxi <swapnilg671@gmail.com>
2024-05-13 01:17:05 +0530

    fruits-app

---
+++ FruitsApp/FruitsApp/View/components/SourceLinkView.swift
@@ -0,0 +1,18 @@
+//
+//  SourceLinkView.swift
+//  FruitsApp
+//
+//  Created by Swap-Dev on 5/12/24.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SourceLinkView()
+}
