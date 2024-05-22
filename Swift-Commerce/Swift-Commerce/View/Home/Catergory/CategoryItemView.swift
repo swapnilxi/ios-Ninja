d6c783f7a643b4b8677cbf26a6a0cbf57b91679d
swapnilxi <swapnilg671@gmail.com>
2024-05-23 00:53:55 +0530

    swift-ecommerce

---
+++ Swift-Commerce/Swift-Commerce/View/Home/Catergory/CategoryItemView.swift
@@ -0,0 +1,46 @@
+//
+//  CategoryItemView.swift
+//  Swift-Commerce
+//
+//  Created by Swap-Dev on 5/19/24.
+//
+
+import SwiftUI
+
+struct CategoryItemView: View {
+    let category: Category
+
+    var body: some View {
+        Button(action: {}, label: {
+          HStack(alignment: .center, spacing: 6) {
+            Image(category.image)
+              .renderingMode(.template)
+              .resizable()
+              .scaledToFit()
+              .frame(width: 30, height: 30, alignment: .center)
+              .foregroundColor(.gray)
+            
+            Text(category.name.uppercased())
+              .fontWeight(.light)
+              .foregroundColor(.gray)
+            
+            Spacer()
+          } //: HSTACK
+          .padding()
+          .background(Color.white.cornerRadius(12))
+          .background(
+            RoundedRectangle(cornerRadius: CGFloat(12))
+              .stroke(Color.gray, lineWidth: 1)
+          )
+        }) //: BUTTON
+    }
+}
+
+#Preview {
+    CategoryItemView(
+        category: categories[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(colorBackground)
+    
+}
