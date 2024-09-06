6f07b4b13a423496b1673f8f6ca912a095bf98e8
swapnil gupta <swapnilgupta.xi@gmail.com>
2024-09-07 02:13:11 +0530

    model-picker

---
+++ RealityKit-ios-learn/RealityKit-ios-learn/RealityKit_ios_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealityKit_ios_learnApp.swift
+//  RealityKit-ios-learn
+//
+//  Created by Abundent on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RealityKit_ios_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
